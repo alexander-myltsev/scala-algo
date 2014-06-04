1b13fd5999864e6c4ff24f3c2afc84f52ea1cb02
Edward Knyshov <edvorg@gmail.com>
2014-06-05 00:09:09 +0400

    pretify

--- src/main/scala/edvorg/algo/Task4.scala
+++ src/main/scala/edvorg/algo/Task4.scala
@@ -5,7 +5,7 @@ import scala.io.Source
 object Task4 extends App {
   var vertexCount = 0
   val graph = Source.fromFile("SCC.txt").getLines.map{ _.split(' ').map{ _.toInt } }
-    .toArray.groupBy{ _.head }.map{ case (k, v) => (k, v map { _.tail.head })}
+    .toArray.groupBy{ _.head }.map{ case (k, v) => (k, v map { _.tail.head }) }
 
   println("Task4")
 }
