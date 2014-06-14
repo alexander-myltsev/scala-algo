02ad09376dc48807959ac4e26c8888fef69fef47
Edward Knyshov <edvorg@gmail.com>
2014-06-14 16:24:58 +0400

    task 6-1 solved

--- src/main/scala/edvorg/algo/Task6_1.scala
+++ src/main/scala/edvorg/algo/Task6_1.scala
@@ -3,15 +3,11 @@ package edvorg.algo
 import io.Source
 
 object Task6_1 extends App {
-  // val arr = Array(1, 2, 3, 4, 5).sorted.map { _.toLong }
-  // val arr = Array(0, 1, 2, 3, -1).sorted.map { _.toLong }
-  val arr = Source.fromFile("algo1_programming_prob_2sum.txt").getLines.map { _.toLong }.toVector.sorted
+  val arr = Source.fromFile("algo1_programming_prob_2sum.txt").getLines.map { _.toLong }
+	.toVector.sorted
 
   val min = - 10000
   val max = 10000
-
-  // val min = -2
-  // val max = 6
 
   var count = 0
   var i = 0
@@ -36,8 +32,6 @@ object Task6_1 extends App {
 	  if (t <= max && !visited(t.toInt - min)) {
 		visited(t.toInt - min) = true
 		count += 1
-		println(s"x ${arr(i)} y ${arr(j)} t ${arr(i) + arr(j)} count $count")
-		println(s"nextj $nextj")
 	  }
 
 	  j -= 1
