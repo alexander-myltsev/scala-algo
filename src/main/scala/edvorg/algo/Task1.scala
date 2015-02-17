a29ba7d51e56ecb27f2301c5f5241e30f1c27331
Alexander Myltsev <alexander.myltsev@phystech.edu>
2015-02-17 22:43:41 +0300

    Dramatically speedup Task1 execution

--- src/main/scala/edvorg/algo/Task1.scala
+++ src/main/scala/edvorg/algo/Task1.scala
@@ -3,10 +3,6 @@ package edvorg.algo
 import scala.annotation.tailrec
 import scala.io.Source
 
-/*
-  1. Elapsed time: 44851932186ns
-
- */
 object Task1 {
   def main(args: Array[String]) {
     val numbers = Source.fromFile("IntegerArray.txt").getLines().toArray.map { _.toInt }
@@ -14,24 +10,26 @@ object Task1 {
     def mergeAndCountInversions(left: Array[Int], right: Array[Int]): (Array[Int], Long) = {
       val mergedArray = Array.fill(left.length + right.length)(0)
       @tailrec
-      def inversions(leftIdx: Int, rightIdx: Int, idx: Int, inv: Long): Long =
-        if (leftIdx == left.length && rightIdx == right.length) inv
-        else if (leftIdx == left.length)
+      def inversions(leftIdx: Int, rightIdx: Int, idx: Int, inv: Long): Long = {
+        val leftEmpty  = leftIdx  == left.length
+        val rightEmpty = rightIdx == right.length
+
+        if (leftEmpty && rightEmpty) inv
+        else if (leftEmpty)
           inversions(leftIdx, rightIdx + 1, { mergedArray(idx) = right(rightIdx); idx + 1 }, inv)
-        else if (rightIdx == right.length)
-          inversions(leftIdx + 1, rightIdx, { mergedArray(idx) = left(leftIdx); idx + 1 }, inv)
-        else if (left(leftIdx) <= right(rightIdx))
+        else if (rightEmpty || left(leftIdx) <= right(rightIdx))
           inversions(leftIdx + 1, rightIdx, { mergedArray(idx) = left(leftIdx); idx + 1 }, inv)
         else
           inversions(leftIdx, rightIdx + 1, { mergedArray(idx) = right(rightIdx); idx + 1 }, inv + left.length - leftIdx)
+      }
 
       (mergedArray, inversions(0, 0, 0, 0))
     }
 
     def countInversions(list: Array[Int]): (Array[Int], Long) = {
-      if (list.isEmpty || list.tail.isEmpty) (list, 0)
+      if (list.size <= 1) (list, 0)
       else {
-        val (left, right) = list splitAt list.length / 2
+        val (left, right) = list.splitAt(list.length / 2)
         val (leftSorted, leftInversions) = countInversions(left)
         val (rightSorted, rightInversions) = countInversions(right)
         val (merged, splitInversions) = mergeAndCountInversions(leftSorted, rightSorted)
